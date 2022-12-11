7d4b46cf67e1b86a9d892976693b283df1ff10fc
Tanner <tanner@imaginarygarage.com>
2022-12-10 21:36:42 -0700

    streamline rx routine

--- stream_rx.py
+++ stream_rx.py
@@ -13,21 +13,11 @@ stream_socket.listen()
 stream, address = stream_socket.accept()
 print(f'Connected to stream from {address}')
 
-rx_data = b''
 while True:
-    while len(rx_data) < packetize.HEADER_SIZE:
-        rx_data += stream.recv(4096)
+    rx_data = stream.recv(packetize.HEADER_SIZE)
     payload_size = packetize.decode_header(rx_data)
-    packet_size = packetize.HEADER_SIZE + payload_size
-
-    while len(rx_data) < packet_size:
-        rx_data += stream.recv(4096)
+    rx_data += stream.recv(payload_size)
     image = packetize.decode_packet(rx_data)
     
     cv2.imshow('rx image', image)
     cv2.waitKey(1)
-
-    rx_data = rx_data[packet_size:]
-
-    if len(rx_data) > 0:
-        print(f"leftovers size: {len(rx_data)}")
